b67632880a4322a914768a1466c854ef6cfb2ceb
mahmoudHammad <41790090+mahmodHammad@users.noreply.github.com>
2020-02-24 21:00:25 +0200

    Updates

---
+++ precache-manifest.beb2574900b01dbe233a757005a2789f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8e43d72abe4643025ffe41060673b4",
+    "url": "/moocHub/index.html"
+  },
+  {
+    "revision": "72a9088b1423d47e9c8b",
+    "url": "/moocHub/static/css/main.2465e21c.chunk.css"
+  },
+  {
+    "revision": "c0d200788a89fa10c909",
+    "url": "/moocHub/static/js/2.84b6c06a.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/moocHub/static/js/2.84b6c06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a9088b1423d47e9c8b",
+    "url": "/moocHub/static/js/main.a5971ef7.chunk.js"
+  },
+  {
+    "revision": "c4677de068c09f19c23d",
+    "url": "/moocHub/static/js/runtime-main.c39fb28a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/moocHub/precache-manifest.2e1bb843e6f3561be8bcd466d73d30d6.js"
+  "/moocHub/precache-manifest.beb2574900b01dbe233a757005a2789f.js"
 );
 
 self.addEventListener('message', (event) => {
