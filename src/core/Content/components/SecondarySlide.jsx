9014add87933005de78ccc3e793860127b4a75da
Mahmoud Hammad <hodaman2012@gmail.com>
2020-04-17 17:54:42 +0200

    adding swipe listener for secondary slide

--- src/core/Content/components/SecondarySlide.jsx
+++ src/core/Content/components/SecondarySlide.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Content from "./ContentCards";
 import Empty from "./Empty";
+import { useSwipeable, Swipeable } from "react-swipeable";
 
 function SecondarySlide({
   content,
@@ -15,27 +16,32 @@ function SecondarySlide({
   className
 }) {
   return (
-    <Grid container className={`${className} cardContent`} justify="center">
-      {content.length ? (
-        content.map((cont, index) => (
-          <React.Fragment key={index}>
-            <Content
-              key={cont.id}
-              content={cont}
-              todo={todo}
-              addToTodo={addToTodo}
-              removeFromTodo={removeFromTodo}
-              subject={subject}
-              Popcontent={Popcontent}
-              isVideo={isVideo}
-              handleVideoPin={handleVideoPin}
-            />
-          </React.Fragment>
-        ))
-      ) : (
-        <Empty />
-      )}
-    </Grid>
+    <Swipeable
+      onSwipedRight={eventData => console.log("swipe right", eventData)}
+      onSwipedLeft={eventData => console.log("swipeleft", eventData)}
+    >
+      <Grid container className={`${className} cardContent`} justify="center">
+        {content.length ? (
+          content.map((cont, index) => (
+            <React.Fragment key={index}>
+              <Content
+                key={cont.id}
+                content={cont}
+                todo={todo}
+                addToTodo={addToTodo}
+                removeFromTodo={removeFromTodo}
+                subject={subject}
+                Popcontent={Popcontent}
+                isVideo={isVideo}
+                handleVideoPin={handleVideoPin}
+              />
+            </React.Fragment>
+          ))
+        ) : (
+          <Empty />
+        )}
+      </Grid>
+    </Swipeable>
   );
 }
 
