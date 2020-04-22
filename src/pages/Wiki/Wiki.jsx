d3cc88fd1bf30425ce23a25039c6d2be726577c2
Mahmoud Hammad <hodaman2012@gmail.com>
2020-04-22 06:46:13 +0200

    displaying text correctly !!!

--- src/pages/Wiki/Wiki.jsx
+++ src/pages/Wiki/Wiki.jsx
@@ -16,65 +16,92 @@ export default class Wiki extends Component {
   state = {
     search: "",
     searchResult: [],
-    title: "",
-    text: "",
     Amper:
-      "{{For|the SI unit of electric current|Ampere}}\n{{short description|River in Germany}}\n{{Expand German|Amper|fa=yes|topic=geo|date=December 2009}}\n{{Infobox river\n| name              = Amper \n| subdivision_type1 = Country\n| subdivision_name1 = [[Germany]] \n| subdivision_type2 = Location\n| subdivision_name2 = [[Bavaria]], [[Germany]] \n| image             = Amper I.jpg \n| image_caption     = The Amper south of [[Fürstenfeldbruck]] \n| source1_location  = [[Northern Limestone Alps]] \n| mouth_location    = [[Isar]] \n| mouth_coordinates = {{coord|48|30|1|N|11|57|24|E|display=inline,title}}\n| progression       = {{RIsar}}\n| length            = {{convert|190|km|abbr=on}} \n| source1_elevation = \n| discharge1_avg    = ±{{convert|45|m3/s|abbr=on}}\n| basin_size        = {{convert|3248|km2|abbr=on}} {{GeoQuelle|DE-BY|GV}}\n}}\n\nThe '''Amper''', called the '''Ammer''' upstream of the [[Ammersee]], through which it runs, is the largest tributary of the [[Isar]] in southern [[Bavaria]], Germany. It flows generally north-eastward, reaching the Isar in [[Moosburg]], about {{convert|190|km}} from its source in the [[Ammergau Alps]], with a flow of 45&nbsp;m³/s. Including its tributary, [[Linder (river)|Linder]], it is {{convert|209.5|km|abbr=on}} long.<ref name=\"DE-BY_GV\" /> Major tributaries are the [[Glonn (Amper)|Glonn]], which springs near [[Augsburg]]; the [[Würm]], which is the outflow of [[Lake Starnberg]]; and the [[Maisach (Amper)|Maisach]].\n\n{{stack|[[File:Flusssystem Ammer Amper.png|thumb|upright|The Ammer/Amper system within the  [[Isar]] basin]]}}\n\nThe Ammer starts just south of the village of [[Oberammergau]]. Riverside cities include [[Fürstenfeldbruck]], [[Dachau]] and Moosburg.\n\n==References==\n{{reflist|30em}}\n\n==External links==\n*{{official website|http://www.wwa-m.bayern.de/fluesse_seen/gewaesserportraits/amper/index.htm}} {{in lang|de}}\n\n[[Category:Amper basin| ]]\n[[Category:Rivers of Bavaria]]\n[[Category:Ammersee]]\n\n{{Bavaria-river-stub}}"
+      "{{For|the SI unit of electric current|Ampere}}\n{{short description|River in Germany}}\n{{Expand German|Amper|fa=yes|topic=geo|date=December 2009}}\n{{Infobox river\n| name              = Amper \n| subdivision_type1 = Country\n| subdivision_name1 = [[Germany]] \n| subdivision_type2 = Location\n| subdivision_name2 = [[Bavaria]], [[Germany]] \n| image             = Amper I.jpg \n| image_caption     = The Amper south of [[Fürstenfeldbruck]] \n| source1_location  = [[Northern Limestone Alps]] \n| mouth_location    = [[Isar]] \n| mouth_coordinates = {{coord|48|30|1|N|11|57|24|E|display=inline,title}}\n| progression       = {{RIsar}}\n| length            = {{convert|190|km|abbr=on}} \n| source1_elevation = \n| discharge1_avg    = ±{{convert|45|m3/s|abbr=on}}\n| basin_size        = {{convert|3248|km2|abbr=on}} {{GeoQuelle|DE-BY|GV}}\n}}\n\nThe '''Amper''', called the '''Ammer''' upstream of the [[Ammersee]], through which it runs, is the largest tributary of the [[Isar]] in southern [[Bavaria]], Germany. It flows generally north-eastward, reaching the Isar in [[Moosburg]], about {{convert|190|km}} from its source in the [[Ammergau Alps]], with a flow of 45&nbsp;m³/s. Including its tributary, [[Linder (river)|Linder]], it is {{convert|209.5|km|abbr=on}} long.<ref name=\"DE-BY_GV\" /> Major tributaries are the [[Glonn (Amper)|Glonn]], which springs near [[Augsburg]]; the [[Würm]], which is the outflow of [[Lake Starnberg]]; and the [[Maisach (Amper)|Maisach]].\n\n{{stack|[[File:Flusssystem Ammer Amper.png|thumb|upright|The Ammer/Amper system within the  [[Isar]] basin]]}}\n\nThe Ammer starts just south of the village of [[Oberammergau]]. Riverside cities include [[Fürstenfeldbruck]], [[Dachau]] and Moosburg.\n\n==References==\n{{reflist|30em}}\n\n==External links==\n*{{official website|http://www.wwa-m.bayern.de/fluesse_seen/gewaesserportraits/amper/index.htm}} {{in lang|de}}\n\n[[Category:Amper basin| ]]\n[[Category:Rivers of Bavaria]]\n[[Category:Ammersee]]\n\n{{Bavaria-river-stub}}",
+    data: false,
+    url: ""
   };
+
+  renderWithStyles(text, links, formatting) {
+    if (links !== undefined) {
+      let pageLinks = links
+        .map(l => {
+          let index = text.indexOf(l.page);
+          return { link: l.page, index };
+        })
+        .filter(pl => pl.index !== -1);
+      let paragraph = "";
+      pageLinks.forEach(pl => {
+        console.log(pl);
+      });
+      console.log("----------");
+    }
+  }
 
   componentDidMount() {
     let doc = this.state.Amper;
-    let jso = wtf(doc).json();
-    let geo = wtf(doc).coordinates();
-    let info = wtf(doc).infobox();
-    let text = wtf(doc).text();
-    let title = wtf(doc).title();
-    let citations = wtf(doc).citations();
-    let data = wtf(doc).data;
-    let debug = wtf(doc).debug();
-    let images = wtf(doc).images();
-    let link = wtf(doc).link();
-    let lists = wtf(doc).lists();
+    let data = wtf(doc).json();
+    // let geo = wtf(doc).coordinates();
+    // let info = wtf(doc).infobox();
+    // let text = wtf(doc).text();
+    // let title = wtf(doc).title();
+    // let citations = wtf(doc).citations();
+    // let data = wtf(doc).data;
+    // let debug = wtf(doc).debug();
+    // let images = wtf(doc).images();
+    // let link = wtf(doc).link();
+    // let lists = wtf(doc).lists();
     let url = wtf(doc).url();
 
-    wtf.fetch("salah").then(data=>{
-      console.log("ddddd",data)
-      this.setState({ text:data.text() ,title:data.title()});
+    // wtf.fetch("salah").then(data=>{
+    //   console.log("ddddd",data)
 
-    }).catch(err=>console.log("errrrrrrrrrrrrrrror"))
+    // }).catch(err=>console.log("errrrrrrrrrrrrrrror"))
 
+    this.setState({ data, url });
 
-    console.log("json", jso);
-    console.log("geo", geo);
-    console.log("info", info);
-    console.log("text", text);
-    console.log("title", title);
-    console.log("citations", citations);
-    console.log("data", data);
-    console.log("debug", debug);
-    console.log("images", images);
-    console.log("link", link);
-    console.log("lists", lists);
-    console.log("url", url);
+    console.log("json", data);
   }
 
   render() {
+    console.log(this.state);
     return (
       <div className="wiki">
         <div className="wikisearch">
-          <Typography variant="h6" align="center">
-            {this.state.title}
-          </Typography>
-          <TextField
+          {/* <TextField
             name="wiki"
             label="Search on WikiPedia"
             variant="outlined"
             onChange={e => this.handleChange(e)}
-          />
+          /> */}
         </div>
-        <Typography variant="body" align="center">
+        {this.state.data !== false ? (
+          <div className="data">
+            <Typography variant="h6" align="center">
+              {this.state.data.title}
+            </Typography>
+            {this.state.data.sections.map(s => (
+              <div className="datasection">
+                {s.paragraphs.map(p => (
+                  <div className="sentence">
+                    {p.sentences.map(s => (
+                      <div className="text">
+                        {this.renderWithStyles(s.text, s.links, s.formatting)}
+                        {s.text}
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>loading...</div>
+        )}
+
+        {/* <Typography variant="body" align="center">
           {this.state.text}
-        </Typography>
+        </Typography> */}
       </div>
     );
   }
