b2ec123c75d1c1d1c349439c22731e5053907efe
Mahmoud Hammad <hodaman2012@gmail.com>
2020-04-23 23:02:35 +0200

    wiki icon

--- src/core/components/Navbar.jsx
+++ src/core/components/Navbar.jsx
@@ -7,7 +7,9 @@ import { Link } from "react-router-dom";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
+import Sports from "@material-ui/icons/Bookmark";
 import { makeStyles } from "@material-ui/core/styles";
+import  Icon  from "@material-ui/core/Icon";
 const useStyles = makeStyles(theme => ({
   logo: {
     flexGrow: 1,
@@ -15,8 +17,12 @@ const useStyles = makeStyles(theme => ({
     fontWeight: "bold"
   },
   study: {
-    padding: " 2px 9px",
-    fontSize: "0.7125rem"
+    padding: " 2px 8px",
+    fontSize: "0.7rem"
+  },
+  wiki: {
+    marginLeft:"5px",
+    color:"#fff"
   }
 }));
 
@@ -63,16 +69,14 @@ export default function Navbar({
           >
             Study Room
           </Button>
-          <Button
+          <Icon
             size="small"
-            className={classes.study}
-            variant="contained"
-            color="secondary"
+            className={classes.wiki}
             component={Link}
             to="/wiki"
           >
-            wiki
-          </Button>
+            <Sports  />
+          </Icon>
         </Toolbar>
       </AppBar>
 
