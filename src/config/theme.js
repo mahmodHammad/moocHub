e5f723b6d81ca3b72ad6f9295818085e95972332
Mahmoud Hammad <hodaman2012@gmail.com>
2020-04-23 23:59:41 +0200

    thems

--- src/config/theme.js
+++ src/config/theme.js
@@ -19,5 +19,30 @@ export default {
     },
     background: {
       default: "#f0f0f0"
+    },
+    s0: {
+      color: "darkred",
+      bg1:" rgb(255, 241, 241)",
+      bg2:" rgb(255, 210, 210)"
+    },
+    s1: {
+      color: "darkgoldenrod",
+      bg1:" rgb(255, 252, 241)",
+      bg2:" rgb(255, 254, 210)"
+    },
+    s2: {
+      color: "darkgreen",
+      bg1:" rgb(244, 255, 241) ",
+      bg2:" rgb(221, 255, 210)"
+    },
+    s3: {
+      color: "darkcyan",
+      bg1:"  rgb(241, 255, 252)",
+      bg2:"rgb(210, 250, 255) "
+    },
+    s4: {
+      color: "darkmagenta",
+      bg1:"  rgb(252, 241, 255)",
+      bg2:"rgb(252, 214, 255) "
     }
   }
--- src/pages/home/components/DisplaySubjects.jsx
+++ src/pages/home/components/DisplaySubjects.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme, props) => ({
+  logo: {
+    flexGrow: 1,
+    justifyContent: "left",
+    fontWeight: "bold"
+  },
+  study: {
+    padding: " 2px 9px", 
+    fontSize: "0.7125rem"
+  },
+  subjectButton: {
+    background: `radial-gradient(ellipse,${theme.palette.s2.bg1}, ${theme.palette.s2.bg2})`,
+    color: theme.palette.s2.color
+  }
+}));
 
 export default function DisplaySubjects({ folder, index }) {
+  const classes = useStyles(`s${index}`);
+  console.log(classes);
   return (
     // What the hell is this
     <div className="subBtn">
@@ -12,10 +31,13 @@ export default function DisplaySubjects({ folder, index }) {
         component={Link}
         to={{
           pathname: `/subject/${folder.name}/${folder.id}`,
-          state: { divided:folder.divided}
+          state: { divided: folder.divided }
         }}
       >
-        <div className={`subjectCard subject${index}`} component="button">
+        <div
+          className={`subjectCard ${classes.subjectButton}`}
+          component="button"
+        >
           {folder.name}
         </div>
       </Button>
