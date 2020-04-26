aadd9bbdccf613247eb92d53a288b99a175285a7
Mahmoud Hammad <hodaman2012@gmail.com>
2020-04-26 16:18:10 +0200

    search-click on search results ,link loads content

--- src/pages/Wiki/Wiki.jsx
+++ src/pages/Wiki/Wiki.jsx
@@ -4,7 +4,7 @@ import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import "./wiki.css";
-import axios from "axios"
+import axios from "axios";
 
 let searchUrl =
   "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=";
@@ -14,7 +14,7 @@ let searchUrl =
 export default class Wiki extends Component {
   state = {
     search: "",
-    searchResult: [],
+    searchResults: [],
     Amper:
       "{{For|the SI unit of electric current|Ampere}}\n{{short description|River in Germany}}\n{{Expand German|Amper|fa=yes|topic=geo|date=December 2009}}\n{{Infobox river\n| name              = Amper \n| subdivision_type1 = Country\n| subdivision_name1 = [[Germany]] \n| subdivision_type2 = Location\n| subdivision_name2 = [[Bavaria]], [[Germany]] \n| image             = Amper I.jpg \n| image_caption     = The Amper south of [[Fürstenfeldbruck]] \n| source1_location  = [[Northern Limestone Alps]] \n| mouth_location    = [[Isar]] \n| mouth_coordinates = {{coord|48|30|1|N|11|57|24|E|display=inline,title}}\n| progression       = {{RIsar}}\n| length            = {{convert|190|km|abbr=on}} \n| source1_elevation = \n| discharge1_avg    = ±{{convert|45|m3/s|abbr=on}}\n| basin_size        = {{convert|3248|km2|abbr=on}} {{GeoQuelle|DE-BY|GV}}\n}}\n\nThe '''Amper''', called the '''Ammer''' upstream of the [[Ammersee]], through which it runs, is the largest tributary of the [[Isar]] in southern [[Bavaria]], Germany. It flows generally north-eastward, reaching the Isar in [[Moosburg]], about {{convert|190|km}} from its source in the [[Ammergau Alps]], with a flow of 45&nbsp;m³/s. Including its tributary, [[Linder (river)|Linder]], it is {{convert|209.5|km|abbr=on}} long.<ref name=\"DE-BY_GV\" /> Major tributaries are the [[Glonn (Amper)|Glonn]], which springs near [[Augsburg]]; the [[Würm]], which is the outflow of [[Lake Starnberg]]; and the [[Maisach (Amper)|Maisach]].\n\n{{stack|[[File:Flusssystem Ammer Amper.png|thumb|upright|The Ammer/Amper system within the  [[Isar]] basin]]}}\n\nThe Ammer '''starts''' just '''south''' of the village of [[Oberammergau]]. '''Riverside''' cities include [[Fürstenfeldbruck]], [[Dachau]] and Moosburg.\n\n==References==\n{{reflist|30em}}\n\n==External links==\n*{{official website|http://www.wwa-m.bayern.de/fluesse_seen/gewaesserportraits/amper/index.htm}} {{in lang|de}}\n\n[[Category:Amper basin| ]]\n[[Category:Rivers of Bavaria]]\n[[Category:Ammersee]]\n\n{{Bavaria-river-stub}}",
     data: false,
@@ -35,12 +35,30 @@ export default class Wiki extends Component {
     }
   };
 
-  handleChange=(e)=>{
-    const value = e.currentTarget.value
-    console.log("hello world")
-    console.log()
-    let searchQuery = searchUrl+value
-    console.log(searchQuery)
+  handleChange = e => {
+    const value = e.currentTarget.value;
+    console.log("hello world");
+    console.log();
+    let searchQuery = searchUrl + value;
+    this.search(searchQuery)
+    console.log(searchQuery);
+  };
+
+  search =url=>{
+    axios
+    .get(url)
+    .then(res => {
+      let names = res.data[1];
+      let urs = res.data[3];
+      let data = [names, urs];
+      this.setState({searchResults:data})
+      console.log(data);
+      console.log("getting data ...");
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
   // XXX Needs Refactoring XXX
   renderWithStyles(text, links, formatting) {
@@ -79,7 +97,7 @@ export default class Wiki extends Component {
         <Typography
           variant="span"
           color="secondary"
-          onClick={() => console.log("hello world", pl)}
+          onClick={() => this.loadContent(pl.word)}
         >
           {pl.word}
         </Typography>
@@ -91,24 +109,25 @@ export default class Wiki extends Component {
     return <div className="modiPh"> {[...paragraph]} </div>;
   }
 
-  componentDidMount() {
-    axios.get(searchUrl).then((res)=>{
-      console.log("getting data ...")
-      console.log(res)
-    }).catch(err=>{
-      console.log(err)
-    })
-    // wtf
-    //   .fetch("salah")
-    //   .then(data => {
-    //     console.log("ddddd", data);
+  loadContent=(title)=>{
+    console.log("getting data form ")
+    wtf
+    .fetch(title)
+    .then(data => {
+      console.log("ddddd", data);
 
-    //     this.setState({ data: data.json(), url: data.url() });
-    //   })
-    //   .catch(err => alert("can not retrieve data!!!"));
+      this.setState({ data: data.json(), url: data.url() });
+    })
+    .catch(err => alert("can not retrieve data!!!"));
+  }
+
+  componentDidMount() {
+  
+  
   }
 
   render() {
+    const {searchResults}=this.state
     return (
       <div className="wiki">
         <div className="wikisearch">
@@ -119,7 +138,14 @@ export default class Wiki extends Component {
             onChange={e => this.handleChange(e)}
           />
         </div>
-        {/* {this.state.data !== false ? (
+        <div className="resluts">
+          {searchResults.length&&searchResults[0].map(e=><div>
+            <Typography onClick={()=>this.loadContent(e)}>{e}</Typography>
+            
+          </div>)}
+        </div>
+
+        {this.state.data !== false ? (
           <div className="data">
             <Typography variant="h6" align="center">
               {this.state.data.title}
@@ -146,7 +172,7 @@ export default class Wiki extends Component {
           </div>
         ) : (
           <LinearProgress color="secondary" />
-        )} */}
+        )}
       </div>
     );
   }
