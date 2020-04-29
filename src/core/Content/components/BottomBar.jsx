8cad5fc9a77835f6378f4870474d124000b60037
Mahmoud Hammad <hodaman2012@gmail.com>
2020-04-29 16:52:15 +0200

    bottom bar swipe

--- src/core/Content/components/BottomBar.jsx
+++ src/core/Content/components/BottomBar.jsx
@@ -4,7 +4,14 @@ import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import DriveEtaIcon from "@material-ui/icons/DriveEta";
+import AcUnit from "@material-ui/icons/AcUnit";
+import Pets from "@material-ui/icons/Pets";
 import { Link } from "react-router-dom";
+import { Swipeable } from "react-swipeable";
+
+import { Redirect } from "react-router-dom";
+
+// to make user see his subjects directly withou choosing his community again
 
 const useStyles = makeStyles({
   root: {
@@ -13,6 +20,11 @@ const useStyles = makeStyles({
     bottom: 0,
     background: "#ffffffaa",
     borderTop: "1px solid #fff"
+  },
+  swip: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center"
   }
 });
 
@@ -24,15 +36,19 @@ export default function SimpleBottomNavigation({
 }) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const [swipe, setswipe] = React.useState(0);
+  // 1 -> study -1 -> home
+
+  if (swipe === 1) {
+    return <Redirect to="/nerds" />;
+  } else if (swipe === -1) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <BottomNavigation
       value={value}
       onChange={(event, newValue) => {
-        if (value === 10) {
-          //   move to videos
-        } else if (value === 100) {
-          //   move to drive (if divided display divided[0] & divided list)
-        }
         if (divided[newValue] !== undefined) {
           loadSubject(divided[newValue].id);
         }
@@ -41,31 +57,38 @@ export default function SimpleBottomNavigation({
       showLabels
       className={classes.root}
     >
-      {divided.map(d => (
-        <BottomNavigationAction label={d.name} key={d.id} />
-      ))}
+      <Swipeable
+        className={classes.swip}
+        onSwipedRight={() => setswipe(-1)}
+        onSwipedLeft={() => setswipe(1)}
+      >
+        {divided.map(d => (
+          <BottomNavigationAction label={d.name} key={d.id} />
+        ))}
 
-      {isVideo ? (
-        <BottomNavigationAction
-          value="100"
-          component={Link}
-          to={{
-            pathname: `/subject/${subject.name}/${subject.id}`,
-            state: { divided}
-          }}
-          icon={<DriveEtaIcon color="secondary" />}
-        />
-      ) : (
-        <BottomNavigationAction
-          value="10"
-          component={Link}
-          to={{
-            pathname: `/videos/${subject.name}/${subject.id}`,
-            state:{divided}
-          }}
-          icon={<YouTubeIcon color="secondary" />}
-        />
-      )}
+        
+        {isVideo ? (
+          <BottomNavigationAction
+            value="100"
+            component={Link}
+            to={{
+              pathname: `/subject/${subject.name}/${subject.id}`,
+              state: { divided }
+            }}
+            icon={<DriveEtaIcon color="secondary" />}
+          />
+        ) : (
+          <BottomNavigationAction
+            value="10"
+            component={Link}
+            to={{
+              pathname: `/videos/${subject.name}/${subject.id}`,
+              state: { divided }
+            }}
+            icon={<YouTubeIcon color="secondary" />}
+          />
+        )}
+      </Swipeable>
     </BottomNavigation>
   );
 }
