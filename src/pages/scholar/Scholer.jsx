5b2c2b5dcb660c8eeafc558c2b8726d76d512c2c
Mahmoud Hammad <hodaman2012@gmail.com>
2020-05-06 07:04:13 +0200

    interpreting then seaching

--- src/pages/scholar/Scholer.jsx
+++ src/pages/scholar/Scholer.jsx
@@ -5,21 +5,48 @@ import React, { Component } from "react";
 import axios from "axios";
 const key = "49dd31f486204254b3dc23dde8c5304c";
 // 4e08ba45eee44bfcb1e10af8c86e0e3d
+const config = {
+  headers: {
+    "Ocp-Apim-Subscription-Key": key
+  }
+};
 export default class componentName extends Component {
-  componentDidMount() {
-    const q =
-      "https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=Composite(AA.AuN=='jaime teevan')&model=latest&count=10&offset=0&attributes=Id";
-    let config = {
-      headers: {
-        "Ocp-Apim-Subscription-Key": "49dd31f486204254b3dc23dde8c5304c"
-      }
-    };
+
+  inter = (query) => {
+    //   composit queries later
+    const search = `https://api.labs.cognitive.microsoft.com/academic/v1.0/interpret?query=DN:${query}&complete=1&count=50`;
     axios
-      .get(q, config)
+      .get(search, config)
       .then(e => {
+        let mod = e.data.interpretations.map(i => {
+            let exp=i.rules[0].output.value;
+            this.search(exp)
+          return exp
+        });
+        console.log(e);
+        console.log(mod);
+      })
+      .then(err => console.log(err));
+  };
+
+  search=exp=>{
+    const search = `https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=${exp}&model=latest&count=10&offset=0&attributes=Ti,Y,CC,AA.AuN,AA.AuId`
+    axios
+      .get(search, config)
+      .then(e => {
+        // let mod = e.data.interpretations.map(i => {
+        //   return i.rules[0].output.value;
+        // });
+        // console.log(mod);
         console.log(e);
       })
       .then(err => console.log(err));
+  }
+
+  componentDidMount() {
+    let query = " atom";
+    this.inter(query)
+
   }
 
   render() {
