9ee24f9be46425bf91585f7b1695c979036e7e25
Mahmoud Hammad <hodaman2012@gmail.com>
2020-05-06 06:00:38 +0200

    setup acadimics api

--- src/pages/scholar/Scholer.jsx
+++ src/pages/scholar/Scholer.jsx
@@ -1,23 +1,28 @@
-import React, { Component } from 'react'
-import scholar from "google-scholar"
+import React, { Component } from "react";
+// key
+// 49dd31f486204254b3dc23dde8c5304c
 
+import axios from "axios";
+const key = "49dd31f486204254b3dc23dde8c5304c";
+// 4e08ba45eee44bfcb1e10af8c86e0e3d
+export default class componentName extends Component {
+  componentDidMount() {
+    const q =
+      "https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=Composite(AA.AuN=='jaime teevan')&model=latest&count=10&offset=0&attributes=Id";
+    let config = {
+      headers: {
+        "Ocp-Apim-Subscription-Key": "49dd31f486204254b3dc23dde8c5304c"
+      }
+    };
+    axios
+      .get(q, config)
+      .then(e => {
+        console.log(e);
+      })
+      .then(err => console.log(err));
+  }
 
-
-export default class Scholer extends Component {
-    componentDidMount() {
-        console.log("searching>>>")
-        scholar.search("chairmouse").then(e=>console.log(e))
-        scholar.search("signal" ).then(e=>{
-            console.log(e)
-        }).catch(err=>console.log(err))
-  
-}
-
-    render() {
-        return (
-            <div>
-                Hello Mother fucker 
-            </div>
-        )
-    }
+  render() {
+    return <div>HELLo</div>;
+  }
 }
